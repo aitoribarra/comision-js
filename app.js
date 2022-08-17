eec8dae6e6f435a19d14c427025537fea9b4da58
omanias <ojmanias1985@gmail.com>
2022-08-16 21:07:21 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+// Aqui voy a mencionar las variables
+/*
+let nombreAlumno = "Juan Pablo"; // string
+console.log(nombreAlumno);
+let edad = 32; // number
+console.log(edad);
+let estudiante = true; // boolean
+console.log(estudiante);
+
+const apellido = "Rodriguez";
+console.log(apellido);
+*/
+
+//Diferencias entre LET y CONST
+
+//Pueden inicializarse vacias
+// pueden ser redeclaras
+/*
+let nombre;
+nombre = "Coder";
+nombre = "House";
+nombre = 37;
+console.log(nombre);
+
+// deben ser declaradas con valor
+// no pueden ser re-declaradas
+const NOMBRE = "Coder";
+
+console.log(NOMBRE);
+
+
+let nombre = "Juan Pablo";
+let apellido = "Rodriguez";
+
+let nombreCompleto = nombre + " " + apellido;
+
+console.log(nombreCompleto);
+
+*/
+
+// -
+// +
+// *
+// /
+
+/*
+let numero1 = 25;
+let numero2 = 35;
+
+let resultado = numero1 / numero2;
+
+console.log(resultado);
+
+*/
+
+//PROMPT
+/*
+let nombre = prompt("Ingrese su nombre");
+
+alert("Hola" + " " + nombre);
+*/
+/*
+// LES DEJO UNA ALTERNATIVA PARA PARSEAR
+let numero1 = Number(prompt("Ingresa el primer numero"));
+let numero2 = Number(prompt("Ingresa el segundo numero"));
+
+let resultado = numero1 + numero2;
+
+alert(resultado);
+
+let numero1 = Number(prompt("Ingrese el primer numero"));
+let numero2 = Number(prompt("Ingrese el segund9o numero"));
+
+let resultado = numero1 + numero2;
+
+alert(resultado);
+
+*/
